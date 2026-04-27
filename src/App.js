463f4b0caeb516f8adecbd485d24d47606e96c71
Débora <dbr_souza@hotmail.com>
2026-04-27 20:29:28 -0300

    Fix: adicionando conteúdo ao App.js para passar nos testes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Projeto DevOps</h1>
+        <p>learn react</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
